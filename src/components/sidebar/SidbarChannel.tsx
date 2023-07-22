26df55cece905bdcd0111572656fbbaaefb3284b
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2023-07-22 18:07:09 +0900

    channelList作成

---
+++ src/components/sidebar/SidbarChannel.tsx
@@ -0,0 +1,12 @@
+import "./SidbarChannel.scss";
+
+export const SidbarChannel = () => {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannelHash">#</span>
+        Udemy
+      </h4>
+    </div>
+  );
+};
